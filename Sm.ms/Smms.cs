f8655ee9974960a5ed6399b1a816bdd4f30f33cb
lindexi <lindexi_gd@163.com>
2021-03-21 19:26:44 +0800

    格式化代码

--- Sm.ms/Smms.cs
+++ Sm.ms/Smms.cs
@@ -21,7 +21,7 @@ namespace Sm.ms
             httpClient.DefaultRequestHeaders.UserAgent.Clear();
             httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0");
 
-          var multipartFormDataContent = new MultipartFormDataContent();
+            var multipartFormDataContent = new MultipartFormDataContent();
             HttpContent fileContent = new StreamContent(image);
             multipartFormDataContent.Add(fileContent, "smfile", fileName);
 
@@ -33,11 +33,11 @@ namespace Sm.ms
 
         public async Task<SmmsResponseBase<UploadImageResponse>> UploadImageAsync(Stream image, string fileName)
         {
-           var json = await UploadImage(image, fileName);
-           return JsonConvert.DeserializeObject<SmmsResponseBase<UploadImageResponse>>(json);
+            var json = await UploadImage(image, fileName);
+            return JsonConvert.DeserializeObject<SmmsResponseBase<UploadImageResponse>>(json);
         }
 
-        public async Task<SmmsResponseBase<UploadImageResponse>> UploadImageAsync(FileInfo imageFile, string fileName=null)
+        public async Task<SmmsResponseBase<UploadImageResponse>> UploadImageAsync(FileInfo imageFile, string fileName = null)
         {
             fileName ??= imageFile.Name;
 
