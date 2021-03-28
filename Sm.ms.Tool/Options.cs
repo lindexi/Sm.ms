f4b96e308cff726f78d209bea2512d236535ebcc
lindexi <lindexi_gd@163.com>
2021-03-28 19:52:12 +0800

    加上命令行

--- Sm.ms.Tool/Options.cs
+++ Sm.ms.Tool/Options.cs
@@ -1,9 +1,13 @@
-﻿namespace Sm.ms.Tool
+﻿using dotnetCampus.Cli;
+
+namespace Sm.ms.Tool
 {
     public class Options
     {
-        public string ApiKey { get; set; }
+        [Option('k', nameof(ApiKey), Description = "密钥，用来上传到sm.ms注册账户下")]
+        public string ApiKey { get; set; } = null!;
 
-        public string FilePath { get; set; }
+        [Option('f', "File", Description = "要上传的图片路径")]
+        public string FilePath { get; set; } = null!;
     }
 }
--- Sm.ms.Tool/Program.cs
+++ Sm.ms.Tool/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using dotnetCampus.Cli;
 
 namespace Sm.ms.Tool
 {
@@ -7,18 +8,23 @@ namespace Sm.ms.Tool
     {
         static void Main(string[] args)
         {
-            Options options = null;
-            if (args.Length == 2)
-            {
-                options = new Options()
-                {
-                    ApiKey = args[0],
-                    FilePath = args[1]
-                };
-            }
+            var commandLine = CommandLine.Parse(args);
+            Options? options = commandLine.As<Options>();
 
             if (options != null)
             {
+                if (string.IsNullOrEmpty(options.FilePath) || !File.Exists(options.FilePath))
+                {
+                    Console.WriteLine($"找不到文件 {options.FilePath}");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(options.ApiKey))
+                {
+                    Console.WriteLine($"没有 API Key 不能上传图片，请到 sm.ms 注册");
+                    return;
+                }
+
                 var smms = new Smms(options.ApiKey);
                 var response = smms.UploadImageAsync(new FileInfo(options.FilePath), Path.GetRandomFileName() + ".png")
                     .Result;
